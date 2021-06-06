b80b8ac8cda20aae72c50d53500b9cd68e28a982
Fred <ricardofredt@hotmail.com>
2021-06-06 15:04:09 -0300

    initial commit

--- src/components/Investments.jsx
+++ src/components/Investments.jsx
@@ -3,15 +3,63 @@ import "./Investments.css"
 
 export default class Investments extends Component {
 	initialState = {
-		Dolars: 0,
+		Dolars: 100000,
+		DolarsInvested: 0,
 		Stock: [],
 		StockValue: [],
-		StockQuantity: [],
+		StockQuantity: [0, 0, 0, 0, 0],
 		StockPL: [],
 		Risk: [],
 	}
-	state = { ...this.initialState }
+	constructor(props) {
+		super(props)
+		this.state = {
+			...this.initialState,
+		}
+		this.HandleBuy = this.HandleBuy.bind(this)
+	}
+	HandleBuy(stockIndex) {
+		if (100 * this.state.StockValue[stockIndex] <= this.state.Dolars) {
+			let StockQuantity = [...this.state.StockQuantity]
 
+			let newQuantity = StockQuantity[stockIndex]
+
+			newQuantity = this.state.StockQuantity[stockIndex] + 100
+
+			StockQuantity[stockIndex] = newQuantity
+
+			this.setState({
+				StockQuantity,
+				Dolars:
+					this.state.Dolars - this.state.StockValue[stockIndex] * 100,
+				DolarsInvested:
+					this.state.DolarsInvested +
+					this.state.StockValue[stockIndex] * 100,
+			})
+		}
+	}
+	HandleSell(stockIndex) {
+		if (this.state.StockQuantity[stockIndex] > 0) {
+			let StockQuantity = [...this.state.StockQuantity]
+
+			let newQuantity = StockQuantity[stockIndex]
+
+			newQuantity = this.state.StockQuantity[stockIndex] - 100
+
+			StockQuantity[stockIndex] = newQuantity
+
+			this.setState({
+				StockQuantity,
+				Dolars:
+					this.state.Dolars + this.state.StockValue[stockIndex] * 100,
+				DolarsInvested:
+					this.state.DolarsInvested -
+					this.state.StockValue[stockIndex] * 100,
+			})
+		}
+	}
+
+	Buy() {}
 	GenerateStockName() {
 		var result = []
 		var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -55,7 +103,7 @@ export default class Investments extends Component {
 
 	componentDidMount() {
 		this.HandleRisk()
-		setTimeout(this.UpdateStocks(), 1)
+		this.UpdateStocks()
 	}
 
 	render() {
@@ -63,126 +111,177 @@ export default class Investments extends Component {
 		let InvestmentBoard
 		if (Kpopers > 10) {
 			InvestmentBoard = (
-				<div className="laptop">
+				//<div className="laptop">
+				<div>
 					<div className="board">
 						<div className="board-row">
-							<div className="board-head">
-								Wallet:
-								<div className="board-row">
+							<div className="board-head">Wallet:</div>
+							<div className="board-row">
+								<div className="board-head">
 									<h4>Available:</h4>
+								</div>
+								<div className="board-head">
+									<h4>Invested:</h4>
+								</div>
+							</div>
+							<div className="board-row">
+								<div className="board-cell">
 									<h5>${this.state.Dolars}</h5>
-									<h5>Invested:</h5>
-									<h5>${this.state.Dolars}</h5>
+								</div>
+								<div className="board-cell">
+									<h5>${this.state.DolarsInvested}</h5>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="board">
-						<div className="board-row">
-							<div className="board-head">Stock</div>
-							<div className="board-head">Price</div>
-							<div className="board-head">Quantity</div>
-							<div className="board-head">P/L</div>
-							<div className="board-head">Operate</div>
-						</div>
-						<div className="board-row">
-							<div className="board-cell">
-								{this.state.Stock[0]}
+						<div className="board">
+							<div className="board-row">
+								<div className="board-head">Stock</div>
+								<div className="board-head">Price</div>
+								<div className="board-head">Quantity</div>
+								<div className="board-head">P/L</div>
+								<div className="board-head">Operate</div>
 							</div>
-							<div className="board-cell">
-								{this.state.StockValue[0]}
+							<div className="board-row">
+								<div className="board-cell">
+									{this.state.Stock[0]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockValue[0]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockQuantity[0]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockPL[0]}
+								</div>
+								<div className="board-cell">
+									<button
+										className="botaoInvestBuy"
+										onClick={() => this.HandleBuy(0)}
+									>
+										Buy
+									</button>
+									<button
+										className="botaoInvestSell"
+										onClick={() => this.HandleSell(0)}
+									>
+										Sell
+									</button>
+								</div>
 							</div>
-							<div className="board-cell">
-								{this.state.StockQuantity[0]}
+							<div className="board-row">
+								<div className="board-cell">
+									{this.state.Stock[1]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockValue[1]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockQuantity[1]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockPL[1]}
+								</div>
+								<div className="board-cell">
+									<button
+										className="botaoInvestBuy"
+										onClick={() => this.HandleBuy(1)}
+									>
+										Buy
+									</button>
+									<button
+										className="botaoInvestSell"
+										onClick={() => this.HandleSell(1)}
+									>
+										Sell
+									</button>
+								</div>
 							</div>
-							<div className="board-cell">
-								{this.state.StockPL[0]}
+							<div className="board-row">
+								<div className="board-cell">
+									{this.state.Stock[2]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockValue[2]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockQuantity[2]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockPL[2]}
+								</div>
+								<div className="board-cell">
+									<button
+										className="botaoInvestBuy"
+										onClick={() => this.HandleBuy(2)}
+									>
+										Buy
+									</button>
+									<button
+										className="botaoInvestSell"
+										onClick={() => this.HandleSell(2)}
+									>
+										Sell
+									</button>
+								</div>
 							</div>
-							<div className="board-cell">
-								<button className="botaoInvestBuy">Buy</button>
-								<button className="botaoInvestSell">
-									Sell
-								</button>
+							<div className="board-row">
+								<div className="board-cell">
+									{this.state.Stock[3]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockValue[3]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockQuantity[3]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockPL[3]}
+								</div>
+								<div className="board-cell">
+									<button
+										className="botaoInvestBuy"
+										onClick={() => this.HandleBuy(3)}
+									>
+										Buy
+									</button>
+									<button
+										className="botaoInvestSell"
+										onClick={() => this.HandleSell(3)}
+									>
+										Sell
+									</button>
+								</div>
 							</div>
-						</div>
-						<div className="board-row">
-							<div className="board-cell">
-								{this.state.Stock[1]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockValue[1]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockQuantity[1]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockPL[1]}
-							</div>
-							<div className="board-cell">
-								<button className="botaoInvestBuy">Buy</button>
-								<button className="botaoInvestSell">
-									Sell
-								</button>
-							</div>
-						</div>
-						<div className="board-row">
-							<div className="board-cell">
-								{this.state.Stock[2]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockValue[2]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockQuantity[2]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockPL[2]}
-							</div>
-							<div className="board-cell">
-								<button className="botaoInvestBuy">Buy</button>
-								<button className="botaoInvestSell">
-									Sell
-								</button>
-							</div>
-						</div>
-						<div className="board-row">
-							<div className="board-cell">
-								{this.state.Stock[3]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockValue[3]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockQuantity[3]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockPL[3]}
-							</div>
-							<div className="board-cell">
-								<button className="botaoInvestBuy">Buy</button>
-								<button className="botaoInvestSell">
-									Sell
-								</button>
-							</div>
-						</div>
-						<div className="board-row">
-							<div className="board-cell">
-								{this.state.Stock[4]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockValue[4]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockQuantity[4]}
-							</div>
-							<div className="board-cell">
-								{this.state.StockPL[4]}
-							</div>
-							<div className="board-cell">
-								<button className="botaoInvestBuy">Buy</button>
-								<button className="botaoInvestSell">
-									Sell
-								</button>
+							<div className="board-row">
+								<div className="board-cell">
+									{this.state.Stock[4]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockValue[4]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockQuantity[4]}
+								</div>
+								<div className="board-cell">
+									{this.state.StockPL[4]}
+								</div>
+								<div className="board-cell">
+									<button
+										className="botaoInvestBuy"
+										onClick={() => this.HandleBuy(4)}
+									>
+										Buy
+									</button>
+									<button
+										className="botaoInvestSell"
+										onClick={() => this.HandleSell(4)}
+									>
+										Sell
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
